0cebfe6afa645466ca98503b2a83f8f7088e2e91
Lisa Meyer <lisamermey@gmail.com>
2022-05-03 14:01:03 -0400

    Load first two stores, update @store1 annual_revenue

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.update(annual_revenue: 350000)
+
+puts @store1.annual_revenue
